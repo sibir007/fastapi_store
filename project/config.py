eeb7ac84bb62befd6c2177c2c75c2745398e72d2
Dmitry Sibiryakov <sibiriakoff2006@yandex.ru>
2026-04-13 21:42:17 +0300

    add config

---
+++ project/config.py
@@ -0,0 +1,37 @@
+# import os
+from pydantic_settings import BaseSettings
+from dotenv import load_dotenv
+
+# for load env vars when run out of docker compose
+ch = load_dotenv(".env")
+
+print(f'load_dotenv: {ch}')
+
+# print(os.environ)
+
+class Settings(BaseSettings):
+    REDIS_HOST: str
+    REDIS_PORT: int
+
+    POSTGRES_HOST: str
+    POSTGRES_PORT: int
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_DB: str
+    # ADMINER_PORT: int
+
+    @property
+    def REDIS_URL(self) -> str:
+        return f"redis://{self.REDIS_HOST}:{self.REDIS_PORT}"
+
+    @property
+    def POSTGRES_URL(self) -> str:
+        return f"postgresql+asyncpg://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@{self.POSTGRES_HOST}:{self.POSTGRES_PORT}/{self.POSTGRES_DB}"
+                    
+
+
+
+
+
+settings = Settings() # type: ignore
+
